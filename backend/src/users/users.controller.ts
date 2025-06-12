82738bd9c3cbd6fa919b5ed91eab0e8295b2e8f3
AmrSayed0 <amracademycode@gmail.com>
2025-06-12 10:08:54 +0300

    feat: secure UsersController with JWT guard

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UsersService } from './users.service';
 
 @Controller('users')
-@UseGuards()
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
